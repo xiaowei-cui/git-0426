8caa22355d693d0ee40cf16ac1c7840886b0f0da
atguigu <atguigu@atguigu.com>
2021-10-02 18:22:51 +0800

    hot-fix commit

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -2,5 +2,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("hello atguigu");
         System.out.println("hello shanghai");
+        System.out.println("hot-fix");
     }
 }
